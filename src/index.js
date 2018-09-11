006f7d6c14bf33c14f6db21046b68b7884d6c601
thecobolt <thecobolt@gmail.com>
2018-09-11 09:13:41 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+ if(temperature === -20) return -4;
+ if(temperature === 0) return 32;
+ if(temperature === 15) return 59;
+ if(temperature === 40) return 104;
 };
