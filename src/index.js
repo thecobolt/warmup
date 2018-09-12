02f56874be534d2fee3857b670dc4c6bdcbc702e
thecobolt <40350524+thecobolt@users.noreply.github.com>
2018-09-12 11:45:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function warmup(temperature) {
- if(temperature === -20) return -4;
- if(temperature === 0) return 32;
- if(temperature === 15) return 59;
- if(temperature === 40) return 104;
+    return temperature*9/5+32;
 };
